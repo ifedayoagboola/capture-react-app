c76a1314c4f03ef2cc3288b3a33cb08d7027ce12
ifedayo agboola <ifedayoagboola19@gmail.com>
2021-02-05 15:57:31 +0100

    3.Services Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import ServicesSection from "./pages/ServicesSection";
 
 function App() {
   return (
     <div className="App">
       <h1>Film</h1>
       <AboutUs />
+      <ServicesSection />
     </div>
   );
 }
